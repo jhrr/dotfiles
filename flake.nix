455fa9434c805cb11e47101d23f4f506713df8b5
jhrr <jhrr@users.noreply.github.com>
2024-04-15 14:49:50 +0100

    Rename default.nix files

--- flake.nix
+++ flake.nix
@@ -4,38 +4,58 @@
   inputs = {
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
       nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      nur.url = "github:nix-community/NUR";
 
-      darwin.url = "github:lnl7/nix-darwin";
-      darwin.inputs.nixpkgs.follows = "nixpkgs";
+      darwin = {
+        url = "github:lnl7/nix-darwin";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
 
-      home-manager.url = "github:nix-community/home-manager";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+      home-manager = {
+        url = "github:nix-community/home-manager";
+        inputs.nixpkgs.follows = "nixpkgs";
+      };
 
       flake-compat = {
         url = "github:edolstra/flake-compat";
         flake = false;
       };
+
       flake-utils = {
         url = "github:numtide/flake-utils";
         inputs.nixpkgs.follows = "nixpkgs";
       };
   };
 
-  outputs = { self, nixpkgs, home-manager, darwin }: {
+  outputs = {
+    darwin,
+    home-manager,
+    nixpkgs,
+    nur,
+    ...
+  }: {
     darwinConfigurations."paradise" = darwin.lib.darwinSystem {
       system = "x86_64-darwin";
       modules = [
+        ./hosts/paradise/darwin.nix
         home-manager.darwinModules.home-manager
-        ./hosts/paradise/default.nix
+        # {
+        #   home-manager.users.jhrr = import ./home.nix;
+        # }
       ];
+      specialArgs = { inherit nixpkgs; };
     };
 
     darwinConfigurations."purgatory" = darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       modules = [
+        ./hosts/purgatory/darwin.nix
         home-manager.darwinModules.home-manager
-        ./hosts/purgatory/default.nix
+        # {
+        #   home-manager.users.jhrr = import ./home.nix;
+        # }
       ];
+      specialArgs = { inherit nixpkgs; };
     };
   };
 }
--- hosts/paradise/darwin.nix
+++ hosts/paradise/darwin.nix
@@ -3,6 +3,12 @@
   services.nix-daemon.enable = true;
   services.nix-daemon.package = pkgs.nixFlakes;
 
+  users = {
+    users.jhrr = {
+      home = /Users/jhrr;
+    };
+  };
+
   homebrew = {
     enable = true;
     autoUpdate = true;
@@ -10,4 +16,8 @@
       # TODO: port from gist
     ];
   };
+
+  # Configure macOS settings.
+  system.defaults.dock.autohide = false;
+  system.defaults.dock.show-recents = false;
 }
