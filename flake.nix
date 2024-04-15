2b55e483153fee3622294a8e14a8569dffea5b7a
jhrr <jhrr@users.noreply.github.com>
2024-04-15 14:39:49 +0100

    Correct hosts in the flake.nix file

--- flake.nix
+++ flake.nix
@@ -5,13 +5,16 @@
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
       nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-      home-manager.url = "github:nix-community/home-manager";
-      home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
       darwin.url = "github:lnl7/nix-darwin";
       darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-      flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+      home-manager.url = "github:nix-community/home-manager";
+      home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+      flake-compat = {
+        url = "github:edolstra/flake-compat";
+        flake = false;
+      };
       flake-utils = {
         url = "github:numtide/flake-utils";
         inputs.nixpkgs.follows = "nixpkgs";
@@ -31,7 +34,7 @@
       system = "aarch64-darwin";
       modules = [
         home-manager.darwinModules.home-manager
-        ./hosts/paradise/default.nix
+        ./hosts/purgatory/default.nix
       ];
     };
   };
